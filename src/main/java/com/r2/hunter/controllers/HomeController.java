1e49f215d05195bcc7e7c113a89b32880b9f3de4
Roman Bondarchuk <rmnbnd@gmail.com>
2016-10-07 15:41:40 +0300

    Start to integrate angular in app

--- src/main/java/com/r2/hunter/controllers/HomeController.java
+++ src/main/java/com/r2/hunter/controllers/HomeController.java
@@ -1,14 +1,8 @@
 package com.r2.hunter.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-    @RequestMapping(value = "/")
-    public String home() {
-        return "HOME PAGE";
-    }
 
 }
